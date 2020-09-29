cdd8d962f3f1c5f67329bf113564f5db135e1a65
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2020-09-29 06:07:58 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Box, ChakraProvider } from '@chakra-ui/core';
+import { ThemeProvider } from 'styled-components';
+import '../styles/globals.css';
+import { theme } from '../theme/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Box as='main'>
+          <Component {...pageProps} />
+        </Box>
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
